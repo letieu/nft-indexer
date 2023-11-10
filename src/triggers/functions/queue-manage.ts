56c638a13293e562b393cc8eb52d1b3cab29bd81
letieu <letieu8@gmail.com>
2023-11-10 16:30:00 +0700

    feat: add save nft queue to report

--- src/triggers/functions/queue-manage.ts
+++ src/triggers/functions/queue-manage.ts
@@ -1,13 +1,15 @@
 import Queue from 'bee-queue';
-import { MetadataData, MintData, QueueNames, queueOptions } from '../../lib/queue';
+import { MetadataData, MintData, NftSaveData, QueueNames, queueOptions } from '../../lib/queue';
 
 const mintQueue = new Queue<MintData>(QueueNames.MINT, queueOptions);
 const metadataQueue = new Queue<MetadataData>(QueueNames.METADATA, queueOptions);
+const nftSaveQueue = new Queue<NftSaveData>(QueueNames.NFT_SAVE, queueOptions);
 
 export async function getQueueReport() {
   // checkhealth
   const mintHealth = await mintQueue.checkHealth();
   const metadataHealth = await metadataQueue.checkHealth();
+  const nftSaveHealth = await nftSaveQueue.checkHealth();
 
   return {
     mint: {
@@ -17,6 +19,10 @@ export async function getQueueReport() {
     metadata: {
       ...metadataHealth,
       queue: QueueNames.METADATA,
+    },
+    nftSave: {
+      ...nftSaveHealth,
+      queue: QueueNames.NFT_SAVE,
     },
   };
 }
